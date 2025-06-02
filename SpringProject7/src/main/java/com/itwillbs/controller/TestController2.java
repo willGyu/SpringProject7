5ea8f264f0dbb909f03130d4bbf0678efaf4ec28
willGyu <jgt8731@itwillbs.co.kr>
2025-06-02 10:31:09 +0900

    TestController2-test2()메서드 작성완료

---
+++ SpringProject7/src/main/java/com/itwillbs/controller/TestController2.java
@@ -0,0 +1,14 @@
+package com.itwillbs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController2 {
+
+	@RequestMapping(value = "/test2")
+	public void test2() {
+		// 기능 완료
+	}
+	
+}
